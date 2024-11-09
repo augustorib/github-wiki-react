6558abc6e04733d5dea224e57b09c03968677c59
augustorib <augustorifra@gmail.com>
2024-11-09 04:48:33 -0300

    feat: criando função buscar repositórios

--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import  {ItemContainer} from "./styles";
 
-function ItemRepo() {
+function ItemRepo({ repo }) {
     return (
         <ItemContainer>
-            <h3>Hello!</h3>
-            <p>world</p>
-            <a href="#">Ver repositório</a><br />
-            <a href="#" className="remover">Remover</a>
+            <h3>{repo.name}</h3>
+            <p>{repo.full_name}</p>
+            <a href={repo.html_url} target="_blank" rel="noreferrer">Ver repositório</a><br />
+            <a href="#" className="remover" rel="noreferrer">Remover</a>
             <hr/>
         </ItemContainer>
     )
