823c59425e63dc999ca433e7299a8cb371be351b
augustorib <augustorifra@gmail.com>
2024-11-09 05:18:29 -0300

    fix: removendo um item da lista ao clicar no link

--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -8,11 +8,11 @@ function ItemRepo({ repo, handleRemoveRepo }) {
     }
 
     return (
-        <ItemContainer onClick={handleRemove}>
+        <ItemContainer >
             <h3>{repo.name}</h3>
             <p>{repo.full_name}</p>
             <a href={repo.html_url} target="_blank" rel="noreferrer">Ver repositório</a><br />
-            <a href="#" className="remover" rel="noreferrer">Remover</a>
+            <a href="#" className="remover" rel="noreferrer" onClick={handleRemove}>Remover</a>
             <hr/>
         </ItemContainer>
     )
